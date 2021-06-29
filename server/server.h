38c4a13ca27fe9ee873f11ef6af4cabfdc258093
Freek Bes <freekbladnl@gmail.com>
2021-06-29 21:08:57 +0200

    removed unneccessary tab

--- server/server.h
+++ server/server.h
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/28 17:50:49 by fbes          #+#    #+#                 */
-/*   Updated: 2021/06/29 21:04:17 by fbes          ########   odam.nl         */
+/*   Updated: 2021/06/29 21:08:34 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 
 typedef struct s_inbox
 {
-	char			*str;
-	char			c;
-}					t_inbox;
+	char		*str;
+	char		c;
+}				t_inbox;
 
-int					ft_strlen(char *str);
-void				write_num(int num);
-void				ft_strcpy(char *src, char *dst);
-t_inbox				*new_inbox(void);
-void				free_inbox(t_inbox *inbox);
-void				comm(int sig);
-void				stop_server(int sig);
+int				ft_strlen(char *str);
+void			write_num(int num);
+void			ft_strcpy(char *src, char *dst);
+t_inbox			*new_inbox(void);
+void			free_inbox(t_inbox *inbox);
+void			comm(int sig);
+void			stop_server(int sig);
 
 #endif
